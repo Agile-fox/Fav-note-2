3b4a17215e26b8b6c2b60185aca4dfa11fc2105f
unknown <jakub@wirtualnafotografia.pl>
2020-02-25 19:42:07 +0100

    adding basic structure of project

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -2,19 +2,19 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: #e6e6e6;
-  width: ${({width}) => width || '220px'};
+  background-color: ${({ theme }) => theme.note};
+  width: 220px;
   height: 47px;
   border: none;
-  border-radius: 50px;
-  font-family: "Montserrat"; 
-  font-weight: 500;
+  border-radius: 50px; 
+  font-family: 'Montserrat'; 
+  font-weight: 600;
   font-size: 16px;
   text-transform: uppercase;
 
   ${({secondary}) => (
     secondary && css`
-    background-color: #ffd82b;
+    background-color: hsl(0, 0%, 90%);
     width: 105px;
     height: 47px;
     font-size: 10px;
@@ -26,3 +26,5 @@ const Button = styled.button`
 
 export default Button;
  
+//  background-color: ${(props) => props.theme.primary}
+//  background-color: ${({theme}) => theme.primary}
---
+++ src/templates/MainTemplate.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'theme/GlobalStyle';
+import { theme } from 'theme/mainTheme';
+import Sidebar from 'components/organisms/Sidebar/Sidebar';
+
+const MainTemplate = ({children}) => (
+  <div>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+        <>
+            <Sidebar />
+            {children}
+        </>
+    </ThemeProvider>
+  </div>
+);
+
+MainTemplate.propTypes = {
+    children: PropTypes.element.isRequired,
+};
+
+export default MainTemplate;
